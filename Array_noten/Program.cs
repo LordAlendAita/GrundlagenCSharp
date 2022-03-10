932800e5116b6251be45aa23355c7d3775602597
alend.i2 <alend.i2@P-0G-0B005-0390>
2022-03-10 12:34:23 +0100

    Projektdateien hinzufügen.

---
+++ Array_noten/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Array_noten
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 20;
+            int[] Noten = new int[n];
+            int summe = 0;
+
+            for (int index = 0; index < n; index++)
+            {
+                Console.WriteLine(index);
+                Noten[index] = Convert.ToInt32(Console.ReadLine());
+                summe = summe + Noten[index];
+            }
+
+            Console.WriteLine(summe / n);
+
+
+            foreach (int s in Noten)
+                Console.WriteLine(s);
+
+            Console.ReadLine();
+        }
+    }
+}
